8b73b66a2417e86b32f12082f902b85d703aa103
Benedek Racz <benedek.racz@propatria-inc.com>
2019-08-26 14:16:12 +0200

    [ADD] minimist CLI argument parse

--- vhdlformat.js
+++ vhdlformat.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = require("fs");
 const process = require("process");
+const minimist = require("minimist");
 const VHDLFormatter_1 = require("./VHDLFormatter");
 ;
 function beautify(input, settings) {
@@ -21,27 +22,18 @@ function beautify(input, settings) {
 }
 function main(options) {
     return new Promise((resolve, reject) => {
-        if (!fs.existsSync(options.input)) {
-            console.error(`-- [ERROR]: could not read filename "${options.input}"`);
+        if (!fs.existsSync(options.inputFile)) {
+            console.error(`-- [ERROR]: could not read filename "${options.inputFile}"`);
             reject(new Error("Could not find file"));
             return;
         }
-        fs.readFile(options.input, (err, data) => {
+        fs.readFile(options.inputFile, (err, data) => {
             if (err != null || ((typeof data) === "undefined")) {
-                console.error(`-- [ERROR]: could not read filename "${options.input}"`);
+                console.error(`-- [ERROR]: could not read filename "${options.inputFile}"`);
                 reject(err);
             }
             const input_vhdl = data.toString('utf8');
-            const settings = new VHDLFormatter_1.BeautifierSettings(false, // removeComments
-            false, // removeReports
-            false, // checkAlias
-            null, // signAlignSettings
-            "uppercase", // keyWordCase
-            "uppercase", // typeCase
-            "\t", // indentation
-            null, // newline settings
-            "\r\n" // end of line
-            );
+            const settings = new VHDLFormatter_1.BeautifierSettings(options.removeComments, options.removeReports, options.checkAlias, options.signAlignSettings, options.keyWordCase, options.typeCase, options.indentation, null, options.endOfLine);
             const result = beautify(input_vhdl, settings);
             if (result.err !== null) {
                 console.error(`-- [ERROR]: could not beautify "${options.input}"`);
@@ -71,35 +63,55 @@ function main(options) {
         });
     });
 }
-;
 function print_usage() {
     console.error(`-- USAGE: node vhdlformat.js [--write] [--debug] [--quiet] <filename 1> [filename 2] ... [filename N]`);
 }
 (() => {
+    const options = {
+        string: ['key-word-case', 'type-case', 'indentation', 'end-of-line', 'inputFiles'],
+        boolean: ['version', 'help', 'overwrite', 'debug', 'quiet', 'version', 'help',
+            'remove-comments', 'remove-reports', 'check-alias'],
+        alias: {
+            v: 'version',
+            h: 'help',
+            removeComments: 'remove-comments',
+            removeReports: 'remove-reports',
+            checkAlias: 'check-alias',
+            keyWordCase: 'key-word-case',
+            typeCase: 'type-case',
+            endOfLine: 'end-of-line',
+        },
+        default: {
+            'version': false,
+            'help': false,
+            'remove-comments': false,
+            'remove-reports': false,
+            'check-alias': false,
+            // sign-align-settings : null,        
+            'key-word-case': "uppercase",
+            'type-case': "uppercase",
+            'indentation': "\t",
+            'end-of-line': "\r\n"
+        }
+    };
+    let args = minimist(process.argv.slice(2), options);
+    args.inputFiles = args._;
+    console.log('args:', args);
     if (process.argv.length < 3) {
         print_usage();
         process.exit(-1);
         return;
     }
-    const filenames = process.argv.filter((arg) => arg.endsWith(".vhd"));
-    if (filenames.length < 1) {
+    if (args.inputFiles.length < 1) {
         print_usage();
         console.error("-- [ERROR]: must specify at least one input filename");
         process.exit(-1);
         return;
     }
-    filenames.forEach((input) => {
-        console.log(`-- [INFO]: Reading ${input}...`);
-        const overwrite = process.argv.includes('--write');
-        const verbose = process.argv.includes("--verbose");
-        const quiet = process.argv.includes("--quiet");
-        main({
-            input,
-            overwrite,
-            verbose,
-            quiet
-        }).catch((err) => {
-            if (verbose) {
+    args.inputFiles.forEach((input) => {
+        args.inputFile = input;
+        main(args).catch((err) => {
+            if (args.verbose) {
                 console.error(err);
             }
         });
--- vhdlformat.ts
+++ vhdlformat.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs';
 import * as process from 'process'
+import * as minimist from 'minimist'
 import { beautify as _beautify, BeautifierSettings } from "./VHDLFormatter";
 
 interface BeautifyStatus {
@@ -22,31 +23,31 @@ function beautify(input: string, settings: BeautifierSettings): BeautifyStatus {
   }
 }
 
-function main(options: CommandOptions): Promise<void> {
+function main(options: any): Promise<void> {
   return new Promise((resolve, reject) => {
-    if (!fs.existsSync(options.input)) {
-      console.error(`-- [ERROR]: could not read filename "${options.input}"`);
+    if (!fs.existsSync(options.inputFile)) {
+      console.error(`-- [ERROR]: could not read filename "${options.inputFile}"`);
       reject(new Error("Could not find file"));
       return;
     }
 
-    fs.readFile(options.input, (err, data) => {
+    fs.readFile(options.inputFile, (err, data) => {
       if (err != null || ((typeof data) === "undefined")) {
-        console.error(`-- [ERROR]: could not read filename "${options.input}"`);
+        console.error(`-- [ERROR]: could not read filename "${options.inputFile}"`);
         reject(err);
       }
       const input_vhdl = data.toString('utf8');
 
       const settings = new BeautifierSettings(
-        false, // removeComments
-        false, // removeReports
-        false, // checkAlias
-        null, // signAlignSettings
-        "uppercase", // keyWordCase
-        "uppercase", // typeCase
-        "\t", // indentation
-        null, // newline settings
-        "\r\n" // end of line
+        options.removeComments,
+        options.removeReports,
+        options.checkAlias,
+        options.signAlignSettings,
+        options.keyWordCase,
+        options.typeCase,
+        options.indentation,
+        null,
+        options.endOfLine,
       );
 
       const result = beautify(input_vhdl, settings);
@@ -83,18 +84,47 @@ function main(options: CommandOptions): Promise<void> {
   });
 }
 
-interface CommandOptions {
-  input: string;
-  overwrite?: boolean;
-  verbose?: boolean;
-  quiet?: boolean;
-};
-
 function print_usage(): void {
   console.error(`-- USAGE: node vhdlformat.js [--write] [--debug] [--quiet] <filename 1> [filename 2] ... [filename N]`);
 }
 
 (() => {
+    
+    
+  const options = {
+    string: ['key-word-case', 'type-case', 'indentation', 'end-of-line', 'inputFiles'],
+    boolean: ['version', 'help', 'overwrite', 'debug', 'quiet', 'version', 'help',
+        'remove-comments', 'remove-reports', 'check-alias'],
+    alias: {
+        v           : 'version',
+        h           : 'help',
+        removeComments: 'remove-comments',
+        removeReports: 'remove-reports',
+        checkAlias  : 'check-alias',
+        keyWordCase : 'key-word-case',
+        typeCase    : 'type-case',
+        endOfLine   : 'end-of-line',
+    },
+    default: {
+        'version'           : false,
+        'help'              : false,
+        'remove-comments'   : false,       
+        'remove-reports'    : false,       
+        'check-alias'       : false,
+        // sign-align-settings : null,        
+        'key-word-case'     : "uppercase", 
+        'type-case'         : "uppercase", 
+        'indentation'       : "\t",
+        'end-of-line'       : "\r\n"  
+    }
+  };
+  
+  let args = minimist(process.argv.slice(2), options);
+  
+  args.inputFiles = args._
+  
+  console.log('args:', args);
+  
 
   if (process.argv.length < 3) {
     print_usage();
@@ -102,31 +132,20 @@ function print_usage(): void {
     return;
   }
 
-  const filenames = process.argv.filter((arg) => arg.endsWith(".vhd"));
-
-  if (filenames.length < 1) {
+  if (args.inputFiles.length < 1) {
     print_usage();
     console.error("-- [ERROR]: must specify at least one input filename")
     process.exit(-1);
     return;
   }
 
-  filenames.forEach((input) => {
-    console.log(`-- [INFO]: Reading ${input}...`)
-    const overwrite = process.argv.includes('--write');
-    const verbose = process.argv.includes("--verbose");
-    const quiet = process.argv.includes("--quiet");
-
-    main({
-      input,
-      overwrite,
-      verbose,
-      quiet
-    }).catch((err) => {
-      if (verbose) {
+  args.inputFiles.forEach((input) => {
+    args.inputFile = input
+    main(args).catch((err) => {
+      if (args.verbose) {
         console.error(err);
       }
     });
-
   })
+
 })();
